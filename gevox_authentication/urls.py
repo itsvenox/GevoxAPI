752d009348b9dd1d7e05553e9f5b508c976a9cfc
Venox <109533093+itsvenox@users.noreply.github.com>
2023-06-25 22:57:21 +0100

    update

---
+++ gevox_authentication/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginAPI),
+    path('signup/', views.signupAPI),
+    path('delete-user/', views.deleteUserAPI)
+]
--- gevox_authentication/views.py
+++ gevox_authentication/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework  import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+
+# Here is the login api request
+@api_view(['POST'])
+def loginAPI(request):
+    return Response({})
+
+
+
+# Here is the signup api request
+@api_view(['POST'])
+def signupAPI(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data["username"])
+        user.set_password(request.data["password"])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({
+            "user": serializer.data,
+            "token": token.key
+        })
+    else:
+        return Response({"error":"User already exist."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Here is the delete user api request
+@api_view(['POST'])
+def deleteUserAPI(request):
+    user = User.objects.get(username=request.data["username"])
+    try:
+        user.delete()
+        return Response({
+            "status":"User deleted successfuly.",
+        })
+    except:
+        return Response({"error":"User not found."},
+                        status=status.HTTP_401_UNAUTHORIZED)
+
+
+
+# @api_view(['POST'])
+# def loginAPI(request):
+#     return Response({})
